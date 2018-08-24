fcddbebd66a396c791e2f4565c3b70e00975cea6
unknown <meettomangesh@gmail.com>
2018-08-24 22:52:59 +0530

    Updated to save work

---
+++ src/Models/Team.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * To Manupulate User table
+ *
+ * 
+ * 
+ */
+
+namespace Api\Models;
+
+use Api\Models\ApiModel;
+use PDO;
+use Redis;
+use Exception;
+use stdClass;
+
+class Team extends ApiModel {
+
+    /**
+     * table name used by model 
+     * @var string
+     */
+    public $tableName = 'team'; //Initialize table name for model
+
+
+
+    /**
+     * Sanitize the values of $params array and return
+     * @param type $params
+     * @return array
+     */
+    private function sanitizeAllData($params = []) {
+        $team = array();
+        $team['id'] = isset($params['id']) ? (int) filter_var($params['id'], FILTER_SANITIZE_NUMBER_INT) : NULL;
+        $team['Username'] = isset($params['Username']) ? filter_var($params['Username'], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES) : '';
+        $team['Balance'] = isset($params['Balance']) ? filter_var($params['Balance'], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES) : '';
+        $team['created_at'] = isset($params['created_at']) ? date('Y-m-d H:i:s', strtotime(filter_var($params['created_at'], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES))) : date('Y-m-d H:i:s');
+        $team['updated_at'] = isset($params['updated_at']) ? date('Y-m-d H:i:s', strtotime(filter_var($params['updated_at'], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES))) : date('Y-m-d H:i:s');
+
+        return $team;
+    }
+
+    /**
+     * The columns of table(STATIC)
+     * @return array
+     */
+    public function getAllTableFields() {
+        return[
+            'id',
+            'Username',
+            'Balance',
+            'created_at',
+            'updated_at'
+        ];
+    }
+
+    /**
+     * check invoice is presented or not
+     * return success response or error response in json 
+     * return id in data params
+     */
+    public function isInvoicePresent($username, $purpose) {
+        try {
+           // $stmt = $pdo->prepare("SELECT * FROM users WHERE id=:id");
+                $stmt = $this->pdo->prepare("SELECT * FROM `invoice` WHERE Purpose=:Purpose AND Username=:Username AND Status='Unpaid' order by id desc limit 1");
+                $stmt->execute(['Purpose' => $purpose,'Username'=>$username]);
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                if ($result) {
+                    return $result;
+                } else {
+                    return 0;
+                }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+}
