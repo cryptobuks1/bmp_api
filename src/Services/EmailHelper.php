48744c47397e6537ae6e3e614ea9916028b10c98
unknown <meettomangesh@gmail.com>
2018-08-30 01:12:29 +0530

    To save work

--- src/Services/EmailHelper.php
+++ src/Services/EmailHelper.php
@@ -1,30 +1,37 @@
 <?php
 
 namespace Api\Services;
+
 use PHPMailer\PHPMailer\PHPMailer;
+
 class EmailHelper {
 
     const DEBUG = true;
+
     public $log = Array();
-    
-    public function sendEmail($fromEmail='support@bitminepool.com',$fromEmailName='Support',$toEmail,$toEmailName,$subject='',$body=''){
-        
-        $mail = new PHPMailer;
-        //To address and name
-        $mail->addAddress($toEmail, $toEmailName);
 
-        //Send HTML or Plain Text email
-        $mail->isHTML(true);
+    public function sendEmail($fromEmail = 'support@bitminepool.com', $fromEmailName = 'Support', $toEmail, $toEmailName, $subject = '', $body = '') {
+        try {
+            $mail = new PHPMailer;
+            //To address and name
+            $mail->addAddress($toEmail, $toEmailName);
 
-        $mail->Subject = $subject;
-        $mail->Body = $body;
-        $mail->AltBody = "This is the plain text version of the email content";
+            //Send HTML or Plain Text email
+            $mail->isHTML(true);
+            $mail->isSMTP();
 
-        if(!$mail->send()) 
-        {
-           return 0; 
-        } else {
-            return 1; 
+            $mail->Subject = $subject;
+            $mail->Body = $body;
+            $mail->AltBody = "This is the plain text version of the email content";
+
+            if (!$mail->send()) {
+                return 0;
+            } else {
+                return 1;
+            }
+        } catch (Exception $e) {
+            return $e->getMessage();
         }
     }
+
 }
